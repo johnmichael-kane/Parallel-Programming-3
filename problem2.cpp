907f3574330ae817ef682dd1e61b5e4454a7fe5c
CandyKaine1 <thejohnmichaelkane@gmail.com>
2024-03-28 22:37:15 -0400

    Added more
    got my foot on the ground and started putting down some includes that i thought i might need as well as setting up some threads

--- problem2.cpp
+++ problem2.cpp
@@ -1,5 +1,15 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <set>
+#include <thread>
+
+#define numThreads 8
+
 /*multicore cpu
-8 temperature sensors
+8 temperature sensors represented as threads
 sensors check at regular intervals, shared memory space
 
 report at the end of every hour, which shows the 
@@ -11,5 +21,10 @@ careful handling, no delays or missing intervals
 
 temp readings are done every minute
 the sensors generate a random number from -100 to 70F
-
 */
+
+int main(){
+    std::vector<int> sensorReadings(numThreads * 60); 
+    std::vector<bool> sensorsReady(numThreads)
+    //the threads only have enough space for 1 hour, if they check every minute
+}
